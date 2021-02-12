70b57c8ba2a0d0063fe6970e754cb49d5c6829ee
MCCHEP <44475776+MCCHEP@users.noreply.github.com>
2021-02-12 19:35:19 +0300

    Поменял порт

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const {
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
